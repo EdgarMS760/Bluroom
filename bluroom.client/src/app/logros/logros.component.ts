6b475050d09c8d48d7f5fdf452fc88808de7d1f8
JoseDelacruzS <jose.delacruzsnt@uanl.edu.mx>
2024-09-08 16:02:29 -0600

    Ver logros completados

--- bluroom.client/src/app/logros/logros.component.ts
+++ bluroom.client/src/app/logros/logros.component.ts
@@ -3,15 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-logros',
   templateUrl: './logros.component.html',
-  styleUrl: './logros.component.css'
+  styleUrls: ['./logros.component.css']
 })
 export class LogrosComponent {
   logros = [
-    { titulo: 'Completa tu primera tarea', icono: 'assets/logros/incompleto/Completa primera tarea_bloqueado.png' },
-    { titulo: 'Completa 5 tareas', icono: 'assets/logros/incompleto/5 tareas_bloqueado.png' },
-    { titulo: 'Comenta en un grupo', icono: 'assets/logros/incompleto/Comenta en un grupo_bloqueado.png' },
-    { titulo: 'Envía 10 mensajes', icono: 'assets/logros/incompleto/envia 10 mensajes_bloqueado.png' },
-    { titulo: 'Envía una imagen', icono: 'assets/logros/complete/envia una imagen.png' },
-    { titulo: 'Haz 2 videollamadas', icono: 'assets/logros/incompleto/2 videollamadas_bloqueado.png' }
+    { titulo: 'Completa tu primera tarea', icono: 'assets/logros/incompleto/Completa primera tarea_bloqueado.png', completed: false },
+    { titulo: 'Completa 5 tareas', icono: 'assets/logros/incompleto/5 tareas_bloqueado.png', completed: false },
+    { titulo: 'Comenta en un grupo', icono: 'assets/logros/incompleto/Comenta en un grupo_bloqueado.png', completed: false },
+    { titulo: 'Envía 10 mensajes', icono: 'assets/logros/incompleto/envia 10 mensajes_bloqueado.png', completed: false },
+    { titulo: 'Envía una imagen', icono: 'assets/logros/complete/envia una imagen.png', completed: true },
+    { titulo: 'Haz 2 videollamadas', icono: 'assets/logros/incompleto/2 videollamadas_bloqueado.png', completed: false }
   ];
 }
