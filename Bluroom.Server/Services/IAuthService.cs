48466056316c7426ba4ce24812cd8401c9644086
Edgar Martinez <edgarsandoval685@gmail.com>
2024-10-20 20:54:50 -0600

    implementado signal R y login y registro

---
+++ Bluroom.Server/Services/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using Bluroom.Server.Dtos;
+using DB;
+
+namespace Bluroom.Server.Services
+{
+    public interface IAuthService
+    {
+        Task<Usuario> Register(UsuarioRegistroDTO registroDto);
+        Task<string> Login(UsuarioLoginDTO loginDto);
+    }
+}
---
+++ bluroom.client/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:5233/api' // Cambia esto por la URL de tu API
+};
