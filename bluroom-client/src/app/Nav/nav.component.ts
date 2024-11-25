2555ab8c55c3b41761d85d8138f1b9d079aa3357
Edgar Martinez <edgarsandoval685@gmail.com>
2024-11-25 14:52:08 -0600

    boton de status funcional

--- bluroom-client/src/app/Nav/nav.component.ts
+++ bluroom-client/src/app/Nav/nav.component.ts
@@ -3,6 +3,7 @@ import { getStorage, ref, getDownloadURL } from '@angular/fire/storage';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { StorageService } from '../services/storageFB.service';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -12,8 +13,9 @@ import { Observable } from 'rxjs';
 export class NavComponent implements OnInit {
   usuario: any = null;
   avatar$: Observable<string> = new Observable();
+  status: true | false = false;
 
-  constructor(private storageService: StorageService) { }
+  constructor(private storageService: StorageService, private authService: AuthService) { }
 
   ngOnInit(): void {
 
@@ -24,9 +26,20 @@ export class NavComponent implements OnInit {
       //this.getImageUrl(this.usuario.avatar);
     }
 
-  
+
   }
-  getImageUrl(avatar : string) {
+  getImageUrl(avatar: string) {
     this.avatar$ = this.storageService.getDownloadURL('avatar/' + avatar + '.png');
+  }
+  setStatus(newStatus: true | false): void {
+    this.status = newStatus;
+
+    const userId = JSON.parse(localStorage.getItem('usuario') || '{}').id;
+    this.authService.updateUserStatus(userId, this.status).then(() => {
+      console.log('Estado del usuario actualizado');
+    }).catch(error => {
+      console.error('Error al actualizar el estado del usuario:', error);
+      alert('No se pudo actualizar el estado del usuario. Por favor, intenta de nuevo.');
+    });
   }
 }
